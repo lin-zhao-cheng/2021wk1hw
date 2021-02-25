9913d2d545e7991ee453c65233d270a98213fd3f
lin-zhao-cheng <jjaacckk41208@gmail.com>
2021-02-25 11:35:16 +0800

    aaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,223 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="main-bgp">
+        <header  id="menu" className="header">
+            <a href="###" style={{'text-decoration': 'none'}}>
+                <img className="header-logo" src="./image/other/game logo.png" alt="LOGO" />
+                <h1 className="wgame">W GAME</h1>
+            </a>
+                
+                <div >
+                    <div className="col">
+                        <img src="./image/other/country.png" alt="language" className="country"/>
+                    </div>
+                    <div className="col">                        
+                        <a href="###">
+                            <img src="./image/other/web.png" alt="member" className="member-p"/>
+                        </a>
+                    </div>
+                </div>
+                <ul className="nav-list">
+                    <li className="nav-list-item"><a href="./news.html" id="topnews"className="nav-list-item-link">最新消息</a></li>
+                    <li className="nav-list-item">
+                        <a id="nav-item1"href="###" className="nav-list-item-link">遊戲</a>
+                        <ul id="nav-item11" className="nav-sub">
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">免費遊戲</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">所有遊戲</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">聯名系列</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">熱門選單</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">未來上市</a></li>
+                        </ul>
+                    </li>
+
+                    <li className="nav-list-item">
+                        <a id="nav-item2" href="###" className="nav-list-item-link">關於</a>
+                        <ul id="nav-item22"className="nav-sub">
+                            <li id="sub"className="nav-list-item"><a href="./about.html" className="nav-list-item-link">關於我們</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">旗下單位</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">徵才計畫</a></li>
+                        </ul>
+                    </li>
+                    <li className="nav-list-item">
+                        <a  id="nav-item3" href="###" className="nav-list-item-link">資源</a>
+                        <ul id="nav-item33" className="nav-sub">
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">客服中心</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">社群論壇</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">投資客</a></li>
+                            <li id="sub"className="nav-list-item"><a href="###" className="nav-list-item-link">媒體窗口</a></li>
+                        </ul>
+                    </li>
+                </ul>
+        </header>
+
+        
+            
+                <div className="main-pitcure">
+                    <div>
+                        <a href="###">
+                            <img id="main-image" src="./image/background/Land by Ann-Nick on DeviantArt.jpg" alt="pure css3 slider" />
+                            <span id="main-info"className="info">LOWPOLY WORLD</span>
+                        </a>
+
+                    </div>
+                    <div>
+                        <a href="###">
+                            <img id="main-image" src="./image/game/afk (2).jpg" alt="pure css3 slider" />
+                            <span id="main-info" className="info">劍與遠征</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="###">
+                            <img id="main-image" src="./image/game/a.jpg" alt="pure css3 slider" />
+                            <span id="main-info" className="info">星際救援</span>
+                        </a>
+
+                    </div>
+                    <div>
+                        <a href="###">
+                            <img id="main-image" src="./image/game/h.jpg" alt="pure css3 slider" />
+                            <span id="main-info" className="info">爺爺的歷險記</span>
+
+                        </a>
+
+                    </div>
+                    <div>
+                        <a href="###">
+                            <img id="main-image" src="./image/game/g.jpg" alt="pure css3 slider" />
+                            <span id="main-info" className="info">戰車世界</span>
+
+                        </a>
+
+                    </div>
+                </div>
+            </div>    
+        <div className="content-area">
+            <article className="article">  
+                <div className="main-area">
+                    <div className="good-game-box">
+                        <h2 className="good-game">精選遊戲</h2>
+                    </div>
+                    <ul id="arow"className="a-row">
+                        <li id="acol" className="a-item-col" data-title="進入官網" >
+                            <a href="###" className="a-item-link">
+                                <img id="col-b" className="pitcure1" src="./image/game/afk.jpg" alt="1"/>
+                            </a>
+                            
+                        </li>
+                        <li id="acol" className="a-item-col" data-title="進入官網" >
+                            <a href="###" className="a-item-link">
+                                <img id="col-b" className="pitcure2" src="./image/game/blue 2.jpg" alt="2"/>
+                            </a>
+                            
+                        </li>
+                        <li id="acol" className="a-item-col" data-title="進入官網" >
+                            <a href="###" className="a-item-link">
+                                <img id="col-b" className="pitcure3" src="./image/game/org.png" alt="3"/>
+                            </a>
+                        </li>
+                        <li id="acol" className="a-item-col" data-title="進入官網" >
+                            <a href="###" className="a-item-link">
+                                <img id="col-b" className="pitcure3" src="./image/game/yow.jpg" alt="3"/>
+                            </a>
+                        </li>
+                        <li id="acol" className="a-item-col" data-title="進入官網" >
+                            <a href="###" className="a-item-link">
+                                <img id="col-b" className="pitcure3" src="./image/game/blue 1.jpg" alt="3"/>
+                            </a>
+                        </li>
+                        <li id="acol" className="a-item-col" data-title="進入官網" >
+                            <a href="###" className="a-item-link">
+                                <img id="col-b" className="pitcure3" src="./image/game/green.png" alt="3"/>
+                            </a>
+                        </li>   
+                    </ul>
+                    <div className="box-game">
+                        <a href="###"className="all-game-link" >
+                           <h3 className="all-game">更多遊戲</h3>
+                        </a>
+                    </div>
+                </div>
+                <aside>    
+            
+                    <div className="other-area">
+                        <div className="p4">
+                            
+                                <img  className="pitcure4" src="image/background/p4.jpg" alt="4"/>
+                                <div className="p4g"></div>
+                            
+                            
+                             <div id="p4iner" className="p4-iner">
+                                <div className="p4-iner-text">
+                                    <p >不只是獲取遊戲而已。從您的遊戲中享受更多樂趣。解鎖專屬獎勵、會員專屬內容和頂級遊戲庫。
+                                    </p>
+                                </div>
+                                <a  href="###" className="p4-iner-btn">
+                                    <h3 >立即加入</h3>
+                                </a>
+                                    
+                             </div>   
+                                
+                            
+                        </div>
+                                            
+                        <div className="undertext">
+                            <div id="col-footer" className="group">
+                                <h3 id="undertitle">關於 W Game</h3>
+                                <p>W Game Studio 是開發、發行與經銷互動式娛樂遊戲與服務的領導廠商，旗下擁有豐富多元的全球知名品牌包括很多你不知道的暢銷作品；遍及全球各地的開發工作室與辦事處團隊致力於在所有熱門遊戲平台包含家用遊樂器主機、行動裝置以及 PC 上，或許還有外星的裝置，我不太想講太多啦!! 反正就是給你帶來最純粹、最深刻的遊戲體驗。</p>
+                            </div>
+                            <div id="col-footer" className="service">
+                                <h3 id="undertitle" >客服相關</h3>
+                                <p>若您有任何遊戲產品相關問題，請參閱我們的 客戶服務中心 專區，在那裡我們提供了詳細的說明來解決您可能遭遇到的問題。如您還需要我們進一步的協助，請依照該專區內的說明填寫回報單與我們的支援團隊聯繫。但是.....不一定會有人理你喔喔喔喔!!!</p>
+                                <div>
+                                    <div className="national">
+                                        <h2 id="undertitle">官方社群</h2>
+                                        <div className="national-icon">
+                                            <a href="###"><img id="icon"src="image/other/facebook.png" alt=""/></a>
+                                            <a href="###"><img  id="icon"src="image/other/instagram.png" alt=""/></a>
+                                            <a href="###"><img  id="icon"src="image/other/youtube.png" alt=""/></a>
+                                            <a href="###"><img  id="icon"src="image/other/twitter.png" alt=""/></a>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div id="col-footer" className="news">
+                                <h3 id="undertitle" className="news-slogan">最新消息</h3>
+                                <ul className="news-list" >
+                                    <li className="news-list-item"><a href="###" className="news-list-link">W GAME與戰艦世界合作，開放全新地圖與關卡</a></li>
+                                    <li className="news-list-item"><a href="###" className="news-list-link">旗下著名遊戲與動畫合作,玩家趕快課金吧!!</a></li>
+                                    <li className="news-list-item"><a href="###" className="news-list-link">旗下地球分部任天堂公司推出全新大亂鬥</a></li>
+                                    <li className="news-list-item"><a href="###" className="news-list-link">全新暗黑俠，將於11月在亞洲服務區推出~</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                 </aside> 
+                 <footer className="footer">
+                    <div className="box-footer">
+                      <p className="footer-v">&copy;2020 - This is the footer.</p>
+                      <a id="ftlogo"className="footer-logo" href="###">
+                        <img src="image/other/game logo.png" className="footer-logo-image" alt=""/>
+                      </a>
+                      
+         
+                    </div>
+                 </footer> 
+            
+            </article>          
+                   
+                
+        
+        
+        </div>
+        
+            
+
     </div>
   );
 }
