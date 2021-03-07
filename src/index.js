3ce759c99b647c799931480003f383af45a3a4bf
lin-zhao-cheng <jjaacckk41208@gmail.com>
2021-03-07 21:17:36 +0800

    ccc

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'
+
 import './css/style.css'
 import './css/normalize.css'
 import './css/all.css'
